8362d633f8dab72c6fa0b6ca990b944aad5d179e
zxm <1260597728@qq.com>
2019-08-01 15:35:52 +0800

    更数据库配置文件

---
+++ application/index/controller/Users.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zxm
+ * Date: 2019/8/1
+ * Time: 2:12 PM
+ */
--- config/database.php
+++ config/database.php
@@ -10,21 +10,21 @@ return [
     //数据库类型
     'type'            => 'mysql',
     //服务器地址
-    'hostname'        => '127.0.0.1',
+    'host'            => '127.0.0.1',
     //数据库名
-    'database'        => '',
+    'database'        => 'th',
     //用户名
-    'username'        => 'wuye',
+    'username'        => 'root',
     //密码
     'password'        => 'root',
     //端口
-    'hostport'        => 'root',
+    'port'            => 3306,
     //连接dsn
-    'dsn'             => '',
+    'dsn'             => '', //mysql:host=127.0.0.1;port=3306;dbname=mysql;charset=utf8
     //数据库连接参数
     'params'          => [],
     //数据库编码默认采用utf8
     'charset'         => 'utf8',
     //数据库表前缀
-    'prefix'          => '',
+    'prefix'          => 'th_',
 ];
